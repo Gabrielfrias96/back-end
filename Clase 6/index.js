6dc24b79338c514fc7545456e3c79d23995f3a13
Gabi Frias <gabifrias@MacBook-Air-de-Ezequiel.local>
2022-01-24 10:11:12 -0300

    Express

--- Clase 6/index.js
+++ Clase 6/index.js
@@ -3,13 +3,13 @@ const req = require('express/lib/request')
 
 const app = express()
 
-const connectServer = app.listen(3000 , ()=>{
+const connectServer = app.listen(8080 , ()=>{
     console.log('Servidor Online')
 })
 
 
 app.get('/', (req , res)=>{
-    res.send('<h1>Hola mundo</h1>')
+    res.send('<h1>Hola mundoooo</h1>')
 } )
 
 app.get('/fyh', (req,res)=> {
@@ -21,9 +21,54 @@ const horario = () =>{
     const horario =  new Date()
     const hora = horario.getHours()
 
-    if (hora > 6  && hora < 13) {return 'Buenos dias'}
+    if (hora > 6  && hora < 13) {return 'Buenos diaaaas'}
     if (hora > 12 && hora < 19) { return 'Buenas tardes'}
     else { return 'Buenas Noches'}
 }
 
 
+let productos = [
+    {
+    id:1,
+    nombre: 'Manzana',
+    color: 'Rojo'
+    },
+    {
+    id:2,
+    nombre: 'Banana',
+    color: 'Amarillo'
+    },
+    {
+    id:3,
+    nombre: 'Pera',
+    color: 'Verde'
+    },
+    {
+    id:4,
+    nombre: 'Mandarina',
+    color: 'Naranja'
+    }
+
+]
+
+
+app.get('/productos', (req,res)=>{
+
+    res.send(JSON.stringify(productos))
+
+})
+
+
+const idRamdom = () => {
+    let idRamdom = Math.floor(Math.random() *(4 - 1) + 1);
+
+    let productRamdom = productos.find(prod => idRamdom === prod.id);
+
+
+    return JSON.stringify(productRamdom)
+
+}
+
+app.get('/productoRandom', (req,res)=>{
+    res.send(idRamdom())
+})
